837db409907bafd926bde57a65e54ae58d9a51fd
AdyC24 <adychandra@gmail.com>
2022-08-12 16:26:25 +0800

    ya gitu lah

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,14 +1,30 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
-
-    public function index(){
-       
-        $this->load->view('login');
+class Auth extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('User_model', 'udel');
     }
-
-    public function home(){
-        $this->load->view('home');
+    // recall saat user baru buka site
+    public function index($session){
+        if($this->session->userdata('authenticated')){
+            redirect('home');
+        } else {
+            $this->load->view('login');
+        }
+    }
+    // recall saat user sudah mengisi form login
+    public function login(){
+        $emp = $this->input->post('NIK');
+        $pass = md5($this->input->post('PASSWORD'));
+        $user = $this->udel->getUser($emp);
+        // BUAT PENGKONDISIAN JIKA USER BELUM ISI FORM (BISA PAKE AJAX DI FORM ATAU FLASHDATA)
+        if($pass == $user->KAR_PASSWORD){
+            $session = array(
+                'authenticated' => true
+            );
+            
+        }
     }
 }
---
+++ application/models/User_Model.php
@@ -0,0 +1,16 @@
+<?php
+class User_Model extends CI_MODEL{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    // [Auth/login]
+    public function getUser($nik){
+        $db1 = $this->load->database('db2', TRUE);
+
+        $db1->from('karyawan');
+        $db1->where($nik);
+        return $db1->get()->row();
+    }
+}
