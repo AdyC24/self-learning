b758b385d7800be0715ba7f0df850aa3d0f721bc
AdyC24 <adychandra@gmail.com>
2022-08-23 09:48:47 +0800

    navbar active

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -17,7 +17,7 @@
                     </span>
                   </a>
                 </li>
-                <?php if($title == 'Movies' || $title == 'Theaters'){
+                <?php if($title == 'Movies' || $title == 'Theaters' || $title == 'Movie Details'){
                   echo '<li class="nav-item dropdown active">';
                 } else {
                   echo '<li class="nav-item dropdown">';
