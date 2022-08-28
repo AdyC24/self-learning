2bc0b70ef656ebe3b9b60893254c01415f784cc3
AdyC24 <adychandra@gmail.com>
2022-08-28 11:15:48 +0800

    perbaiki navbar

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -68,21 +68,9 @@
                     </span>
                   </a>
                 </li>
-                <?php if($title == 'Competences'){
-                  echo '<li class="nav-item active">';
-                } else {
-                  echo '<li class="nav-item">';
-                }
-                ?>
-                <a class="nav-link" href="<?=base_url('Page/competences');?>" >
-                        <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
-                        </span>
-                        <span class="nav-link-title">
-                          Competences
-                        </span>
-                      </a>
-                    </li>
+
+                
+
                 <?php if($role != 'lvl345'){?>
                   <?php if($role != 'lvl12') { ?>
                     <li class="nav-item dropdown">
@@ -117,18 +105,41 @@
                   };?>
                       <a class="nav-link" href="<?= base_url('Page/subordinate');?>" >
                         <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
-                        </span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-affiliate" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M5.931 6.936l1.275 4.249m5.607 5.609l4.251 1.275"></path>
+                        <path d="M11.683 12.317l5.759 -5.759"></path>
+                        <circle cx="5.5" cy="5.5" r="1.5"></circle>
+                        <circle cx="18.5" cy="5.5" r="1.5"></circle>
+                        <circle cx="18.5" cy="18.5" r="1.5"></circle>
+                        <circle cx="8.5" cy="15.5" r="4.5"></circle>
+                      </svg>    
+                      </span>
                         <span class="nav-link-title">
                           Subordinates
                         </span>
                       </a>
                     </li>
                 <?php };?>
-
+                
+                <?php if($title == 'Competences'){
+                  echo '<li class="nav-item active">';
+                } else {
+                  echo '<li class="nav-item">';
+                }
+                ?>
+                <a class="nav-link" href="<?=base_url('Page/competences');?>" >
+                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/star -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
+                    </span>
+                    <span class="nav-link-title">
+                      Competences
+                    </span>
+                  </a>
+                </li>
                 
                 <li class="nav-item">
-                  <a class="nav-link" href="./docs/index.html" >
+                  <a class="nav-link" href="#" >
                     <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/file-text -->
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><line x1="9" y1="9" x2="10" y2="9" /><line x1="9" y1="13" x2="15" y2="13" /><line x1="9" y1="17" x2="15" y2="17" /></svg>
                     </span>
