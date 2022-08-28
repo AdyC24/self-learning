9f447521dab6731220a3cb263b487338b30ab6bb
AdyC24 <adychandra@gmail.com>
2022-08-28 15:06:11 +0800

    delete add movie di role direct

--- application/views/page/movie.php
+++ application/views/page/movie.php
@@ -22,11 +22,12 @@
                       </span>
                     </div>
                   </div>
-                  <a href="#" class="btn btn-primary">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>
-                    Add movie
-                  </a>
+                  <?php if($role == 'hr'){
+                    echo '<a href="#" class="btn btn-primary">';
+                    echo '<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>';
+                    echo 'Add movie';
+                    echo '</a>';
+                  };?>
                 </div>
               </div>
             </div>
